aa7bdd5ebcae97615d31b6ee5cab5f5e70b67517
abdulazizthemodernworker@gmail.com <abdulazizthemodernworker@gmail.com>
2024-01-21 23:01:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
+import "./App.css";
+import { Modal } from "./components/Modal";
+
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleButtonClick = (value) => {
+    setModalOpen(false);
+    setMessage(value);
+  };
+
+  return (
+    <div className="App">
+      {message}
+      <button className="btn btn-open" onClick={() => setModalOpen(true)}>
+        Open
+      </button>
+      {modalOpen &&
+        createPortal(
+          <Modal
+            closeModal={handleButtonClick}
+            onSubmit={handleButtonClick}
+            onCancel={handleButtonClick}
+          >
+            <br />
+          </Modal>,
+          document.body
+        )}
+    </div>
+  );
+}
+
+export default App;
